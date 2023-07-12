df49ec73d4788ef3d938421be1e65ff941a24ad9
Valeriereds <valeriereds@gmail.com>
2023-07-11 21:54:17 -0500

    added beginning bootstrap components to html

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// variables for api key and url
+// var for city to collect the user search input
+// use a for city variable and appid  for the api key to look up 
+// we'll need 2 urls: one for the 5day forecast {lat/long}
+// 2nd url will be needed for the city info to input for the city data
+var keyMe = "373d8c572f14892cf8f19bb961710ac8"
+var cordLink = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=373d8c572f14892cf8f19bb961710ac8"
+var cityLink = "https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=373d8c572f14892cf8f19bb961710ac8"
+
+// api link for each city
+// var link for long /lat
+// var city url
+// user inputs city it goes into cityurl
+// from cityurl well take the lat/long
+
+
+// fetch function for api
+// we need the city lat/long 
+
+
+// search button click event listener
+// function to save user search to local storage
+// function to render it to page
+
+
+// need a conditional function for day.js to render weather by date/time accurately on the page
+// need to have the api linked for long/lat of at least 5 cities/zipcodes
+// can we do this for all cities instead of just named cities?
+// request url variable 
